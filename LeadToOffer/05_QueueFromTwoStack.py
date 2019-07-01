a49ad681123e2de980f1867870aa782c65ff1359
lawy623 <lawy623@gmail.com>
2019-06-30 23:13:58 -0700

    q5 queue from two stacks

---
+++ LeadToOffer/05_QueueFromTwoStack.py
@@ -0,0 +1,17 @@
+## python version.
+class Solution:
+    def __init__(self):
+        self.stack1 = []
+        self.stack2 = []
+
+    def push(self, node):
+        # write code here
+        self.stack1.append(node)
+    def pop(self):
+        # return xx
+        while len(self.stack1) != 0:
+            self.stack2.append(self.stack1.pop())
+        res = self.stack2.pop()
+        while len(self.stack2) != 0:
+            self.stack1.append(self.stack2.pop())
+        return res
