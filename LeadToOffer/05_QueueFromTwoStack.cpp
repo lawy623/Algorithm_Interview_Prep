d0b5d1da2fededad1bdb3e3d4d4877ae35e29931
lawy623 <lawy623@gmail.com>
2019-07-01 20:42:46 -0700

    modify solution

--- LeadToOffer/05_QueueFromTwoStack.cpp
+++ LeadToOffer/05_QueueFromTwoStack.cpp
@@ -26,3 +26,18 @@ private:
     stack<int> stack1;
     stack<int> stack2;
 };
+
+//2. You don't need to do it every time. Just pop stack1 if stack2 if empty.
+int pop() {
+    if (stack2.empty()) {
+        while(!stack1.empty()) {
+            stack2.push(stack1.top());
+            stack1.pop();
+        }
+    }
+
+    int res = stack2.top();
+    stack2.pop();
+
+    return res;
+}
--- LeadToOffer/05_QueueFromTwoStack.py
+++ LeadToOffer/05_QueueFromTwoStack.py
@@ -15,3 +15,12 @@ class Solution:
         while len(self.stack2) != 0:
             self.stack1.append(self.stack2.pop())
         return res
+
+## No need to pop every time
+    def pop(self):
+        if len(self.stack2) == 0:
+            while len(self.stack1) != 0:
+                self.stack2.append(self.stack1.pop())
+        res = self.stack2.pop()
+
+        return res
