e7373276e31ab13aaa5ef2e69aa3a48d8fe623a1
lawy623 <lawy623@gmail.com>
2019-07-07 22:11:49 -0700

    q22 tree level print

--- LeadToOffer/22_BinaryTreeLevelPrint.py
+++ LeadToOffer/22_BinaryTreeLevelPrint.py
@@ -1,24 +1,19 @@
-// Use Queue for level print
-class Solution {
-public:
-    vector<int> PrintFromTopToBottom(TreeNode* root) {
-        vector<int> res;
-        if (!root)
-            return res;
+## Use Queue for level print
+class Solution:
+    # 返回从上到下每个节点值列表，例：[1,2,3]
+    def PrintFromTopToBottom(self, root):
+        if root is None:
+            return []
 
-        queue<TreeNode*> vals;
-        vals.push(root);
-        TreeNode* temp;
-        while(!vals.empty()) {
-            temp = vals.front();
-            vals.pop();
-            res.push_back(temp->val);
-            if (temp->left)
-                vals.push(temp->left);
-            if (temp->right)
-                vals.push(temp->right);
-        }
-
-        return res;
-    }
-};
+        res = []
+        queue = []
+        queue.append(root)
+        while len(queue)>0:
+            x = queue[0]
+            queue.pop(0)
+            res.append(x.val)
+            if x.left is not None:
+                queue.append(x.left)
+            if x.right is not None:
+                queue.append(x.right)
+        return res
