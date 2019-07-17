7a54c61fc5a7c19a7cf8ddcc32b7c239ff60730d
lawy623 <lawy623@gmail.com>
2019-07-16 22:53:31 -0700

    first common node

---
+++ LeadToOffer/36_FirstCommonNode.cpp
@@ -0,0 +1,56 @@
+//1. Use map. O(m+n) time. O(n) space.
+class Solution {
+public:
+    ListNode* FindFirstCommonNode( ListNode* pHead1, ListNode* pHead2) {
+        if (!pHead1 || !pHead2)
+            return NULL;
+        unordered_map<ListNode*, int> map;
+        while(pHead1) {
+            map[pHead1] = 0;
+            pHead1 = pHead1->next;
+        }
+        while(pHead2) {
+            if (map.find(pHead2)!=map.end())
+                return pHead2;
+            pHead2 = pHead2->next;
+        }
+        return NULL;
+    }
+};
+
+//2. The part after common are the same. Let the fast go by diff, then compare the two list with same length.
+//O(n) time, O(1) space.
+class Solution {
+public:
+    ListNode* FindFirstCommonNode( ListNode* pHead1, ListNode* pHead2) {
+        if (!pHead1 || !pHead2)
+            return NULL;
+        ListNode* p1 = pHead1;
+        ListNode* p2 = pHead2;
+        int len1 = 0, len2 = 0;
+        while(p1) {
+            len1++;
+            p1 = p1->next;
+        }
+        while(p2){
+            len2++;
+            p2 = p2->next;
+        }
+        p1 = pHead1;
+        p2 = pHead2;
+        if (len1>len2) {
+            while(len1-- >len2)
+                p1 = p1->next;
+        } else if (len1<len2){
+            while(len2-- >len1)
+                p2 = p2->next;
+        }
+        while(p1){
+            if (p1==p2)
+                return p1;
+            p1 = p1->next;
+            p2 = p2->next;
+        }
+        return NULL;
+    }
+};
