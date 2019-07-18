ade8e95e727d11601114ff3b19bc22e6effe4fe7
lawy623 <lawy623@gmail.com>
2019-07-18 15:09:11 -0700

    q46 by deduction

--- LeadToOffer/46_LastNumInCircle.cpp
+++ LeadToOffer/46_LastNumInCircle.cpp
@@ -25,3 +25,23 @@ public:
         return circle.front();
     }
 };
+
+
+
+//2. By deduction
+class Solution {
+public:
+    int LastRemaining_Solution(int n, int m)
+    {
+        if (n<=0 || m<=0)
+            return -1;
+        if (m==1)
+            return n-1;
+
+        int f_n_m = 0;
+	for(int i = 1; i < m; i++) {
+	    f_n_m = (f_n_m + m) % n;
+	}
+	return f_n_m;
+    }
+};
