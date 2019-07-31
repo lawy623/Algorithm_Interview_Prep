7260bd8410e47285f0c8c4c0419dd958a52a398e
lawy623 <lawy623@gmail.com>
2019-07-31 00:20:06 -0700

    better solution less space

--- Algo/LeadToOffer/20_MinInStack.cpp
+++ Algo/LeadToOffer/20_MinInStack.cpp
@@ -22,3 +22,27 @@ private:
     stack<int> vals;
     stack<int> mins;
 };
+
+// 2. You do not need to push all the same mins.
+class Solution {
+public:
+    void push(int value) {
+        vals.push(value);
+        if (mins.empty() || mins.top()>=value)
+            mins.push(value);
+    }
+    void pop() {
+        if (vals.top() == mins.top())
+            mins.pop();
+        vals.pop();
+    }
+    int top() {
+        return vals.top();
+    }
+    int min() {
+        return mins.top();
+    }
+private:
+    stack<int> vals;
+    stack<int> mins;
+};
