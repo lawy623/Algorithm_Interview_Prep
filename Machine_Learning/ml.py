9f9651710bdfe6c18c796b7d6c4f593045ab7a0d
lawy623 <lawy623@gmail.com>
2019-08-07 18:17:45 -0700

    add iou calculation

--- Machine_Learning/ml.py
+++ Machine_Learning/ml.py
@@ -1,6 +1,6 @@
 def softmax(x):
     """ Softmax function """
-    return x / np.sum(x.exp())
+    return x.exp() / np.sum(x.exp())
 
 def softmax_modify(x):
     """ the input x may be too large. """
@@ -26,3 +26,20 @@ def corr2d(X, K):
 		for j in range(Y.shape[1]):
 			Y[i,j] = (X[i:i+h, j:j+w]).sum()
 	return Y
+
+def comp_iou(r1, r2):
+    """ rec: (y0, x0, y1, x1) """
+    s1 = (r1[3]-r1[1])*(r1[2]-r1[0])
+    s1 = (r2[3]-r2[1])*(r2[2]-r2[0])
+    s_sum = s1+s2
+
+    left = max(r1[1], r2[1]) ## intersect's left, is the rightmost x0
+    right = min(r1[3], r2[3]) ## intersect's right, is the leftmost x1
+    top = max(r1[0], r2[0]) ## intersect's top, is the bottom y0
+    bottom = min(r1[2], r2[2]) ## intersect's bottom, is the top of y1
+
+    if left>=right or top>=bottom:
+        return 0 ## No overlap
+    else:
+        intercept = (right-left)*(bottom-top)
+        return intercept / (s_sum-intercept)
