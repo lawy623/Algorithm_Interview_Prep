ef237898d0b2d0357555a6a0ac800a2449643cf8
leoyluo <leoyluo@tencent.com>
2021-08-17 15:00:01 +0800

    short practice

--- Algo/Leetcode/226InvertBinaryTree.cpp
+++ Algo/Leetcode/226InvertBinaryTree.cpp
@@ -1,3 +1,4 @@
+// recursive
 class Solution {
 public:
     TreeNode* mirrorTree(TreeNode* root) {
@@ -7,5 +8,23 @@ public:
         root->left = mirrorTree(r);
         root->right = mirrorTree(l);
         return root;
+    }
+};
+
+// Use queue
+class Solution {
+public:
+    TreeNode* mirrorTree(TreeNode* root) {
+        if (!root) return root;
+        queue<TreeNode*> q;
+        q.push(root);
+        while(!q.empty()){
+            TreeNode* t = q.front();
+            swap(t->left, t->right);
+            if(t->left) q.push(t->left);
+            if(t->right) q.push(t->right);
+            q.pop();
+        }
+        return q;
     }
 };
